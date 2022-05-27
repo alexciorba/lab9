aa9bdd2b1f94237346acf810c3855d01800c986e
alexciorba <alexciorba28@gmail.com>
2022-05-27 22:12:07 +0300

    first commit

---
+++ Common/MemoryMappedTypes/TypeOfGeo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum TypeOfGeo : int
+{
+        highway = 0,
+        water,
+        railway,
+        natural,
+        boundary,
+        landuse,
+        residential,
+        farm,
+        reservoir,
+        building,
+        leisure,
+        amenity,
+        name,
+        place,
+        admin_level
+}
